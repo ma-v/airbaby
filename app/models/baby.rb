45bf155e1bbbc522e899417233b9023a444ec921
Mathias Allagnat <mathiasallagnat@gmail.com>
2019-08-13 16:20:01 +0200

    validations tested, create baby done

--- app/models/baby.rb
+++ app/models/baby.rb
@@ -2,7 +2,7 @@ class Baby < ApplicationRecord
   belongs_to :user
   validates :first_name, presence: { message: "Enter a cutie name for your cutie baby" }, format: { with: /[a-zA-Z]/ }
   validates :title, presence: { message: "Enter a cutie nickname for your cutie baby" }, format: { with: /[a-zA-Z]/ }
-  validates :gender, presence: { message: "Your baby can be a male a female or ?" }, inclusion: { in: ["male", "female", "?"] }
+  validates :gender, presence: { message: "Your baby can be a male a female or ?" }, inclusion: { in: ["male", "female", "other"] }
   validates :description, presence: { message: "Please enter at least 10 characters :)" }, length: { minimum: 3 }
   validates :age, presence: { message: "Enter a month from 0 to 36, before and after well it's not a baby ;)" }, inclusion: { in: [0..36] }
   validates :price_per_day, presence: { message: "Enter a number, don't be greedy but don't be cheap either, you are renting your baby ffs" }
