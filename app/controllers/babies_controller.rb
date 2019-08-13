8526024f80580da291097828dfb517b23db8a782
Mathias Allagnat <mathiasallagnat@gmail.com>
2019-08-13 15:38:42 +0200

    new action created, view new form created

--- app/controllers/babies_controller.rb
+++ app/controllers/babies_controller.rb
@@ -1,2 +1,21 @@
 class BabiesController < ApplicationController
+  def new
+    @baby = Baby.new
+    authorize @baby
+  end
+
+  def create
+    @baby = Baby.new(babies_params)
+    authorize @baby
+    @baby.user = current_user
+    if @baby.save
+      redirect_to root_path
+    else
+      redirect_to render: :new
+    end
+  end
+
+  def babies_params
+    params.require(:baby).permit(:first_name, :photo, :age, :gender, :description, :price_per_day, :clean, :sleepy, :excited, :crying, :fat, :playful, :funny, :public, :animals, :speaks, :title)
+  end
 end
