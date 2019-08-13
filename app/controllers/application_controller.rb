7ba641f0f030d4d360898f581eb85532dd11a3b1
Mathias Allagnat <mathiasallagnat@gmail.com>
2019-08-13 12:21:40 +0200

    permit access to upload photo at creation or edition of user profile

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,9 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
+
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:username, :address, :description, :pedophile])
-    devise_parameter_sanitizer.permit(:account_update, keys: [:username, :address, :description])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:username, :address, :description, :pedophile, :photo])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:username, :address, :description, :photo])
   end
 end
