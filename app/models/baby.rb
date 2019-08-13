7302675b7d19fc2d464f8e68f2b8c6a4d6a8b346
Mavrick Munoz <mavrick.munoz@gmail.com>
2019-08-13 17:50:57 +0200

    added the photo to the creation of baby

--- app/models/baby.rb
+++ app/models/baby.rb
@@ -1,4 +1,5 @@
 class Baby < ApplicationRecord
+  mount_uploader :photo, PhotoUploader
   belongs_to :user
   validates :first_name, presence: { message: "Enter a cutie name for your cutie baby" }, format: { with: /[a-zA-Z]/ }
   validates :title, presence: { message: "Enter a cutie nickname for your cutie baby" }, format: { with: /[a-zA-Z]/ }
@@ -6,5 +7,6 @@ class Baby < ApplicationRecord
   validates :description, presence: { message: "Please enter at least 10 characters :)" }, length: { minimum: 10 }
   validates :age, presence: true
   validates :price_per_day, presence: { message: "Enter a number, don't be greedy but don't be cheap either, you are renting your baby ffs" }
+  validates :photo, presence: { message: "Please put a pic of your cutie baby" }
   validates_numericality_of :price_per_day
 end
