db31fac566b79209caaa1eae3104a00b244469b0
Mavrick Munoz <mavrick.munoz@gmail.com>
2019-08-13 15:58:40 +0200

    Show page done without front

--- app/controllers/babies_controller.rb
+++ app/controllers/babies_controller.rb
@@ -1,2 +1,13 @@
 class BabiesController < ApplicationController
+  before_action :set_baby, only: [:show]
+
+  def show
+  end
+
+  private
+
+  def set_baby
+    @baby = Baby.find(params[:id])
+    authorize @baby
+  end
 end
