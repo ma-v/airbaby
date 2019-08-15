8dc25340023acea49e1bf4a22838e157f776bc70
Mavrick Munoz <mavrick.munoz@gmail.com>
2019-08-15 18:02:24 +0200

    implemented filters for age and for price

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,6 +3,8 @@ class PagesController < ApplicationController
 
   def home
     @babies = policy_scope(Baby)
+    @babies = policy_scope(Baby.where("age >= :min_age AND age <= :max_age", { min_age: params[:min_age], max_age: params[:max_age] })) if (params[:min_age].present? && params[:min_age].present?)
+    @babies = policy_scope(Baby.where("price_per_day >= :min_price AND price_per_day <= :max_price", { min_price: params[:min_price], max_price: params[:max_price] })) if (params[:min_price].present? && params[:max_price].present?)
     @markers = @babies.map do |baby|
       {
         lat: baby.user.latitude,
