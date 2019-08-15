aa369658e06eeb29b8342bd37f3d2c99eb2fc6e5
Bonnand Baptiste <baptiste.bonnand@yahoo.fr>
2019-08-15 14:46:26 +0200

    map flying to markers

--- app/javascript/plugins/init_mapbox.js
+++ app/javascript/plugins/init_mapbox.js
@@ -10,18 +10,41 @@ const initMapbox = () => {
 
   if (mapElement) { // only build a map if there's a div#map to inject into
     mapboxgl.accessToken = mapElement.dataset.mapboxApiKey;
-  const map = new mapboxgl.Map({
-    container: 'map',
-    style: 'mapbox://styles/mathias2189/cjzbakq3y015a1clf7t2jj7kn'
-  });
-  const markers = JSON.parse(mapElement.dataset.markers);
-  markers.forEach((marker) => {
-    new mapboxgl.Marker()
-    .setLngLat([ marker.lng, marker.lat ])
-    .addTo(map);
-  });
-  fitMapToMarkers(map, markers);
-}
+    const map = new mapboxgl.Map({
+      container: 'map',
+      style: 'mapbox://styles/mathias2189/cjzbakq3y015a1clf7t2jj7kn'
+    });
+
+    const markers = JSON.parse(mapElement.dataset.markers);
+    markers.forEach((marker) => {
+      const popup = new mapboxgl.Popup().setHTML(marker.infoWindow);
+      const element = document.createElement('div');
+       element.style.backgroundImage = `url('${marker.image_url}')`;
+       element.className = 'marker';
+       element.style.backgroundSize = 'contain';
+       element.style.width = '35px';
+       element.style.height = '35px';
+      new mapboxgl.Marker(element)
+      .setLngLat([ marker.lng, marker.lat ])
+      .setPopup(popup)
+      .addTo(map);
+    });
+
+    fitMapToMarkers(map, markers);
+
+    const cardProducts = document.querySelectorAll('.card-product');
+    if (cardProducts.length) {
+      cardProducts.forEach((card) => {
+        card.addEventListener('mouseover', () => {
+          map.flyTo({
+            center: JSON.parse(card.dataset.center),
+            speed: 2,
+            zoom: 15
+          });
+        });
+      })
+    }
+  }
 };
 
 export { initMapbox };
