3dfeb1105d398eaec8745fa3d6dd7ca8379f1432
Mavrick Munoz <mavrick.munoz@gmail.com>
2019-08-15 15:07:31 +0200

    end date disabled if startdate.blank?, submit button displays price according to time range chosen, dashboard shows total price on bookings

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,5 +2,10 @@ import "bootstrap";
 import 'mapbox-gl/dist/mapbox-gl.css'; // <-- you need to uncomment the stylesheet_pack_tag in the layout!
 
 import { initMapbox } from '../plugins/init_mapbox';
+import { initBookingForm } from '../components/booking_form';
 
 initMapbox();
+initBookingForm();
+
+
+
