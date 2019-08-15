1c49f7d55ac22899a8edf40b32ec68a3cceab1d5
Michael Brunel <brunel.michael@gmail.com>
2019-08-16 01:29:16 +0200

    style for cards in dashboard done

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -4,9 +4,11 @@ import '@mapbox/mapbox-gl-geocoder/dist/mapbox-gl-geocoder.css';
 
 import { initMapbox } from '../plugins/init_mapbox';
 import { initBookingForm } from '../components/booking_form';
+import { initDashboardTabs } from '../components/dashboard_tabs';
 
 initMapbox();
 initBookingForm();
+initDashboardTabs()
 
 
 
