ab77ee312eeca3964969c11dbee36efe54f70356
Michael Brunel <brunel.michael@gmail.com>
2019-08-16 14:11:18 +0200

    booking not possible if end date before start date without javascript

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -4,10 +4,17 @@ class Booking < ApplicationRecord
   validates :start_date, presence: true
   validates :end_date, presence: true
   validates :price, presence: true
-
+  validate :end_date_cannot_be_lower_than_start_date
   validate :baby_available?
 
   def baby_available?
     errors.add(:start_date, "Baby is already taken at these dates") unless baby.available?(start_date, end_date)
   end
+
+  def end_date_cannot_be_lower_than_start_date
+    if start_date > end_date
+      errors.add(:end_date, "can't be before starting date")
+    end
+  end
 end
+
